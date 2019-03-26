153072d241e06bfc4da0836bd1b73a6abac24d83
Holger <horol18@student.sdu.dk>
2019-03-26 12:45:45 +0100

    Prototype 0.1.2

--- Prototype 0.1/js/script.js
+++ Prototype 0.1/js/script.js
@@ -1,7 +1,15 @@
 var stage;
 
 function init() {
+    canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
     stage = new createjs.Stage(document.getElementById('canvas'));
+    
+    var backGround = new createjs.Shape(new createjs.Graphics().beginLinearGradientFill(["#333", "#ffffff"], [0, 1], -0, 200, 0, 800).drawRect(-0, -0, canvas.width, canvas.height));
+    stage.addChild(backGround);
+    
     createjs.Ticker.addEventListener("tick", handleTick);
     createjs.Ticker.setFPS(60);
     start();
