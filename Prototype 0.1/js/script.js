ce42343531582a763a72fb97223ee47bd5f2dd0a
Holger <horol18@student.sdu.dk>
2019-03-26 21:58:41 +0100

    Prototype 0.2
    Big changes

--- Prototype 0.1/js/script.js
+++ Prototype 0.1/js/script.js
@@ -1,39 +1,60 @@
-    var stage;
+$(function () {
 
-    function init() {
-        canvas = document.getElementById("canvas");
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+    var nav = $('.navbar'),
+        doc = $(document),
+        win = $(window);
 
-        stage = new createjs.Stage(document.getElementById('canvas'));
+    win.scroll(function () {
 
-        var backGround = new createjs.Shape(new createjs.Graphics().beginLinearGradientFill(["#333", "#ffffff"], [0, 1], -0, 200, 0, 800).drawRect(-0, -0, canvas.width, canvas.height));
-        stage.addChild(backGround);
+        if (doc.scrollTop() > 80) {
+            nav.addClass('scrolled');
+        } else {
+            nav.removeClass('scrolled');
+        }
 
-        createjs.Ticker.addEventListener("tick", handleTick);
-        createjs.Ticker.setFPS(60);
-        start();
-    }
+    });
 
-    function handleTick(e) {
-        stage.update();
-    }
+    // Trigger the scroll listener on page load
 
-    function start() {
+    win.scroll();
+});
 
-        //LOGO
-        var logo = new createjs.Bitmap('img/logo.png');
-        logo.x = 50;
-        logo.y = 50;
-        logo.scaleX = 0.2;
-        logo.scaleY = 0.2;
-        stage.addChild(logo);
-        
-        //NAVN
-        var navn = new createjs.Text("Pheonix Klip", "bold 120px sans-serif", "#FF0000")
-        navn.x = 410;
-        navn.y = 180;
-        navn.textBaseline = "alphabetic";
-        /*navn.shadow = new createjs.Shadow('#000', -15, 25, 18);*/
-        stage.addChild(navn)
-    }
+var stage;
+
+function init() {
+    canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    stage = new createjs.Stage(document.getElementById('canvas'));
+
+    var backGround = new createjs.Shape(new createjs.Graphics().beginLinearGradientFill(["#333", "#ffffff"], [0, 1], -0, 200, 0, 800).drawRect(-0, -0, canvas.width, canvas.height));
+    stage.addChild(backGround);
+
+    createjs.Ticker.addEventListener("tick", handleTick);
+    createjs.Ticker.setFPS(60);
+    start();
+}
+
+function handleTick(e) {
+    stage.update();
+}
+
+function start() {
+
+    //LOGO
+    var logo = new createjs.Bitmap('img/logo.png');
+    logo.x = 50;
+    logo.y = 50;
+    logo.scaleX = 0.2;
+    logo.scaleY = 0.2;
+    stage.addChild(logo);
+
+    //NAVN
+    var navn = new createjs.Text("Pheonix Klip", "bold 120px sans-serif", "#FF0000")
+    navn.x = 410;
+    navn.y = 180;
+    navn.textBaseline = "alphabetic";
+    /*navn.shadow = new createjs.Shadow('#000', -15, 25, 18);*/
+    stage.addChild(navn)
+}
