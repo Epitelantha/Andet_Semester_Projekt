53685de427131c41f2b2488c8545005cb32c9f76
Holger <horol18@student.sdu.dk>
2019-03-28 10:46:53 +0100

    Prototype 0.2.5
    FUCK THE BORDER

--- Prototype 0.2/js/script.js
+++ Prototype 0.2/js/script.js
@@ -24,7 +24,7 @@ $(function () {
     bckground.height(function(){
         
         windowHeight = win.innerHeight();
-        bckground.css('height', windowHeight - 144);
+        bckground.css('height', windowHeight - 134);
         
     });
     
