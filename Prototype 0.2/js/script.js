7ae6a5e00d0f610645403d985c1a6b5161ef0956
Holger <horol18@student.sdu.dk>
2019-03-28 09:39:10 +0100

    Prototype 0.2.4
    Response

--- Prototype 0.2/js/script.js
+++ Prototype 0.2/js/script.js
@@ -4,6 +4,8 @@ $(function () {
         doc = $(document),
         win = $(window),
         ico = $('.fa');
+    
+    var bckground = $('.headpicture')
 
     win.scroll(function () {
 
@@ -18,48 +20,18 @@ $(function () {
         }
 
     });
+    
+    bckground.height(function(){
+        
+        windowHeight = win.innerHeight();
+        bckground.css('height', windowHeight - 144);
+        
+    });
+    
+    bckground.height();
 
     // Trigger the scroll listener on page load
 
     win.scroll();
+    
 });
-
-var stage;
-
-function init() {
-    canvas = document.getElementById("canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    stage = new createjs.Stage(document.getElementById('canvas'));
-
-    var backGround = new createjs.Shape(new createjs.Graphics().beginLinearGradientFill(["#333", "#ffffff"], [0, 1], -0, 200, 0, 800).drawRect(-0, -0, canvas.width, canvas.height));
-    stage.addChild(backGround);
-
-    createjs.Ticker.addEventListener("tick", handleTick);
-    createjs.Ticker.setFPS(60);
-    start();
-}
-
-function handleTick(e) {
-    stage.update();
-}
-
-function start() {
-
-    //LOGO
-    var logo = new createjs.Bitmap('img/logo.png');
-    logo.x = 50;
-    logo.y = 50;
-    logo.scaleX = 0.2;
-    logo.scaleY = 0.2;
-    stage.addChild(logo);
-
-    //NAVN
-    var navn = new createjs.Text("Pheonix Klip", "bold 120px sans-serif", "#FF0000")
-    navn.x = 410;
-    navn.y = 180;
-    navn.textBaseline = "alphabetic";
-    /*navn.shadow = new createjs.Shadow('#000', -15, 25, 18);*/
-    stage.addChild(navn)
-}
