e43028f1c130984f4c3bad87b398cb0bb4f58d31
Holger Roland <horol18@student.sdu.dk>
2019-04-09 07:43:04 +0200

    Søndergade

---
+++ Prototype Salon Søndergade/js/changeColor.js
@@ -0,0 +1,6 @@
+$(function () {
+
+    $('.active').css('background-color', '#5383d3'),
+    $('.active:hover').css('background-color', '#9393f9'),
+    $('.dropdown:hover').css('background-color', '#5383d3');
+});
