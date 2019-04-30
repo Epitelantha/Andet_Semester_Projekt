93cb1618d80c75316e9e042a348bb6de40079b3e
Holger <horol18@student.sdu.dk>
2019-04-30 11:55:58 +0200

    Extracted

---
+++ Prototype 0.237/Prototype 0.237/js/database.js
@@ -0,0 +1,19 @@
+function Database() {
+    
+};
+
+Database.prototype.create = function() {
+    
+};
+
+Database.prototype.list = function() {
+    
+};
+
+Database.prototype.delete = function() {
+    
+};
+
+Database.prototype.update = function() {
+    
+};
