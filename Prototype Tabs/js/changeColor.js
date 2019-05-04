a88582b3949bd3be8e0357541e496dd5e06b80cd
Holger <horol18@student.sdu.dk>
2019-05-04 14:40:20 +0200

    ColorChanging

--- Prototype Tabs/js/changeColor.js
+++ Prototype Tabs/js/changeColor.js
@@ -3,6 +3,30 @@ var secondaryColor = '#ff5623';
 var backgroundColor = '#442c2f';
 
 $(function () {
+    
+    if (localStorage.getItem("mainColor") != null) {
+        mainColor = localStorage.getItem("mainColor");
+        console.log(mainColor);
+    }
+    if (localStorage.getItem("secondaryColor") != null) {
+        secondaryColor = localStorage.getItem("secondaryColor");
+    }
+    if (localStorage.getItem("backgroundColor") != null) {
+        backgroundColor = localStorage.getItem("backgroundColor");
+    }
+    
+
+    if (localStorage.getItem("mainColor") == null) {
+        localStorage.setItem("mainColor", mainColor);
+    }
+    if (localStorage.getItem("secondaryColor") == null) {
+        localStorage.setItem("secondaryColor", secondaryColor);
+    }
+    if (localStorage.getItem("backgroundColor") == null) {
+        localStorage.setItem("backgroundColor", backgroundColor);
+    }
+    
+    
 
     $('.header-user-dropdown .header-limiter h1 span').css('color', mainColor),
         $('.header-user-dropdown .header-limiter .header-user-menu').css('background-color', secondaryColor),
