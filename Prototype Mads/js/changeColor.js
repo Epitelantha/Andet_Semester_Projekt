8757c4395ea74753a3fdd98261cc2a35d109ddf4
Holger <horol18@student.sdu.dk>
2019-05-05 11:42:33 +0200

    Spring cleaning

--- Prototype Mads/js/changeColor.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-
-    $('.active').css('background-color', '#5383d3'),
-    $('.active:hover').css('background-color', '#9393f9'),
-    $('.dropdown:hover').css('background-color', '#5383d3');
-});
